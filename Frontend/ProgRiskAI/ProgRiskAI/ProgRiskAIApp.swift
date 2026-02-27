44557a92fec2b603c93f8768dac5b5d26de1c3f2
Ryan Nair <ryannair05@gmail.com>
2026-02-27 17:45:59 -0500

    ProgRiskAIApp

---
+++ Frontend/ProgRiskAI/ProgRiskAI/ProgRiskAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgRiskAIApp.swift
+//  ProgRiskAI
+//
+//  Created by Ryan Nair on 2/27/26.
+//
+
+import SwiftUI
+
+@main
+struct ProgRiskAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
